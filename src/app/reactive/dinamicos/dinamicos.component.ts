c630a6a576ddf171dec6afa879cb67c159045ee6
unknown <sergio-caballero_student2021@wilder.school>
2021-09-26 21:52:15 +0200

    formArray added in favoritos

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -11,14 +11,19 @@ export class DinamicosComponent implements OnInit {
 
   miFormulario: FormGroup= this.fb.group({
     nombre :      ['', [Validators.required, Validators.minLength(3)]],
+    favoritos: this.fb.array([['Super Mario Bros', Validators.required], ['GTA V', Validators.required]], Validators.required) 
    
   })
+
+  get favoritosArr(){
+    return this.miFormulario.get('favoritos') as FormArray
+  }
 
   constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
   }
-  
+
   campoNoEsValido(campo:string){
     return this.miFormulario.controls[campo].errors 
            && this.miFormulario.controls[campo].touched
