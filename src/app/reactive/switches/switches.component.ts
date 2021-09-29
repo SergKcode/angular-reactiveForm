00a75e6239e2ec2df7eaf2fa323f3a102fc8d950
unknown <sergio-caballero_student2021@wilder.school>
2021-09-29 20:00:13 +0200

    Switches reactive form done

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { format } from 'path';
 
 @Component({
   selector: 'app-switches',
@@ -25,7 +26,21 @@ export class SwitchesComponent {
   constructor( private fb:FormBuilder) { }
 
   ngOnInit(): void {
-    this.miFormulario.reset({...this.persona, condiciones:false})
+    this.miFormulario.reset({
+      ...this.persona, 
+      condiciones:false
+    });
+
+    /* this.miFormulario.get('condiciones')?.valueChanges.subscribe(newValue=>console.log(newValue)) *///para extraer un solo valor
+    this.miFormulario.valueChanges.subscribe( ({condiciones, ...restoArgumentos}) =>{
+      this.persona=restoArgumentos;
+    })
+  }
+  guardar(){
+    const formValue={...this.miFormulario.value}
+    delete formValue.condiciones
+
+    this.persona=formValue
   }
 
 }
